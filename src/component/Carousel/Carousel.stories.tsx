dc2999e45064ec999906df9b8c1dc650ed0802b0
simform-deep <deepak.p@simformsolutions.com>
2023-06-05 10:20:55 +0530

    Fix: Carousel layout changes

--- src/component/Carousel/Carousel.stories.tsx
+++ src/component/Carousel/Carousel.stories.tsx
@@ -1,6 +1,8 @@
+import React, { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-
-import { Carousel } from 'antd';
+import type { RadioChangeEvent } from 'antd';
+import { Carousel, Radio } from 'antd';
+import type { DotPosition } from 'antd/es/carousel';
 
 const contentStyle: React.CSSProperties = {
     margin: 0,
@@ -22,20 +24,113 @@ export default meta;
 
 type Story = StoryObj<typeof Carousel>;
 
-export const BasicCarousel: Story = {
-  args:{
-    autoplay:true,
-    children:<div><div>
-    <h3 style={contentStyle}>1</h3>
-  </div>
-  <div>
-    <h3 style={contentStyle}>2</h3>
-  </div>
-  <div>
-    <h3 style={contentStyle}>3</h3>
-  </div>
-  <div>
-    <h3 style={contentStyle}>4</h3>
-  </div></div>
-  } ,
+const CarouselBasic = () => {
+  const onChange = (currentSlide: number) => {
+    console.log(currentSlide);
+  };
+  return <>
+        <Carousel afterChange={onChange}>
+            <div>
+              <h3 style={contentStyle}>1</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>2</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>3</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>4</h3>
+            </div>
+        </Carousel>
+      </>
 };
+
+const CarouselDots = () => {
+  const [dotPosition, setDotPosition] = useState<DotPosition>('top');
+
+  const handlePositionChange = ({ target: { value } }: RadioChangeEvent) => {
+    setDotPosition(value);
+  };
+
+  return <>
+        <Radio.Group onChange={handlePositionChange} value={dotPosition} style={{ marginBottom: 8 }}>
+        <Radio.Button value="top">Top</Radio.Button>
+        <Radio.Button value="bottom">Bottom</Radio.Button>
+        <Radio.Button value="left">Left</Radio.Button>
+        <Radio.Button value="right">Right</Radio.Button>
+      </Radio.Group>
+      <Carousel dotPosition={dotPosition}>
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </>
+};
+
+
+
+const AutomaticallyScroll = () => {
+  return <>
+        <Carousel autoplay>
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
+      </>
+};
+
+
+const CarouseFadein = () => {
+  return <>
+        <Carousel effect="fade">
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
+      </>
+};
+
+export const BasicCarousel: Story ={
+  render: () => <CarouselBasic />,
+}
+
+export const DotsCarousel: Story ={
+  render: () => <CarouselDots />,
+}
+
+export const ScrollAutomatically: Story ={
+  render: () => <AutomaticallyScroll />,
+}
+
+export const FadeinCarouse: Story ={
+  render: () => <CarouseFadein />,
+}
+
